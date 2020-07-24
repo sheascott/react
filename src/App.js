5ddc0748820e4a0b40b85bb87a6df19dc6cf87b8
Shea Scott <sheatscott@gmail.com>
2020-07-24 11:41:41 -0600

    Changes.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
+import Radium, {StyleRoot} from 'radium';
 import Person from './Person/Person';
+
 
 class App extends Component {
   state = {
@@ -50,7 +52,11 @@ class App extends Component {
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     }
 
 
@@ -72,22 +78,37 @@ if (this.state.showPersons) {
       })}
     </div>
   )
-  style.backgroundColor = 'red';
   dispVariable = 'Hide Persons'
+  style.backgroundColor = 'red';
+  style[':hover'] = {
+    backgroundColor: 'lightred',
+    color: 'black'
+  }
 }
+
+const classes = [];
+if (this.state.persons.length <= 2){
+  classes.push('red'); //classes = ['red']
+}
+if (this.state.persons.length <= 1){
+  classes.push('bold'); //classes = ['red', bold']
+}
+
     return (
+      <StyleRoot>
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
+        <p className={classes.join(' ')}>This is really working!</p>
         <button 
           style = {style}
           onClick={this.togglePersonsHandler}>{dispVariable}
           </button>
         {persons}
       </div>
+      </StyleRoot>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
-export default App;
+export default Radium(App);
